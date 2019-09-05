0f876b4e844e4bca48a151a13edc985cd61e45fe
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-05 15:01:02 -0700

    ensure that all buttons are present on Dashboard

--- baseball-scoreboard/src/App.test.js
+++ baseball-scoreboard/src/App.test.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render, fireEvent } from '@testing-library/react'
+import React from 'react'
+import DashBoard from './components/Dashboard'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+test('Strike button is present', () => {
+  const { getByText } = render(<DashBoard />)
+  const strike = getByText("Strike")
+  expect(strike).toBeDefined()
+}) 
+
+test('Ball button is present', () => {
+  const { getByText } = render(<DashBoard />)
+  const ball = getByText("Ball")
+  expect(ball).toBeDefined()
+})
+
+test('Foul button is present', () => {
+  const { getByText } = render(<DashBoard />)
+  const foul = getByText("Foul")
+  expect(foul).toBeDefined()
+})
+
+test('Hit button is present', () => {
+  const { getByText } = render(<DashBoard />)
+  const hit = getByText("Foul")
+  expect(hit).toBeDefined()
+})
+
+
