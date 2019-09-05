585d4f6972281084350bbd99188758aabd9639b4
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-05 13:02:17 -0700

    add and render Dashboard component

---
+++ baseball-scoreboard/src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Dashboard(props) {
+    return (
+        <div className="dashboard">
+            <button>Strike</button>
+            <button>Ball</button>
+            <button>Foul</button>
+            <button>Hit</button>
+        </div>
+    )
+}
